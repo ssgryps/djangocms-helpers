3055d2e65142a1a5a303d0236972d0e6e19cf8cc
Victor Yunenko <victor@what.digital>
2020-03-05 19:49:02 +0200

    add email vars

--- djangocms_helpers/utils/send_email.py
+++ djangocms_helpers/utils/send_email.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django_settings_export import settings_export
@@ -26,6 +27,8 @@ def send_email(
 
 def _get_full_context(context_base: dict = None) -> dict:
     settings_exported = settings_export(request=None)
+    settings_exported['site'] = Site.objects.get_current()
+    settings_exported['base_url'] = settings.META_SITE_PROTOCOL + Site.objects.get_current().domain
     if context_base is None:
         return settings_exported
     else:
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='djangocms-helpers',
-    version='1.0.2',
+    version='1.1.0',
     author='Victor',
     author_email='victor@what.digital',
     url='https://gitlab.com/what-digital/djangocms-helpers',
@@ -16,5 +16,6 @@ setup(
         'django >= 2.2, < 3',
         'django-cms >= 3.7, < 4.0',
         'django-fieldsignals >= 0.4.0',
+        'django-meta',
     ],
 )
