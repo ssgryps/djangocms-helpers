77d2ccd5ac9b394429706cfb05b9608c027440ca
Victor Yunenko <victor@what.digital>
2020-06-08 19:47:55 +0300

    handle cms abscence in installed apps

--- djangocms_helpers/apps.py
+++ djangocms_helpers/apps.py
@@ -1,5 +1,3 @@
-import importlib
-
 from django.apps import AppConfig
 
 
@@ -8,7 +6,7 @@ class DjangoCmsHelpersConfig(AppConfig):
 
     # noinspection PyUnresolvedReferences
     def ready(self):
-        is_django_cms_installed = importlib.util.find_spec('cms') is not None
+        is_django_cms_installed = 'cms' in settings.INSTALLED_APPS
         if is_django_cms_installed:
             from djangocms_helpers.monkey_patches.static_placeholder_publishing_fix import monkeypatch_cms_has_publish_permission
             from djangocms_helpers.fixes.language_slug_fix import fix_page_slugs
